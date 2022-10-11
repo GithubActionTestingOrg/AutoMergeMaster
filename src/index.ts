0bedf13afc2986c70cefb0a294be2f37c60fd914
corbie1 <bratchikovjohn@gmail.com>
2022-10-11 13:48:19 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,23 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-const { Octokit } = require("@octokit/rest");
 
 const token: string = core.getInput('token')
-const labels: string[] = JSON.parse(core.getInput('labels'))
-const skipSec: Number = parseInt(core.getInput('skip_hour')) * 60 * 60
 const repoOwner: string = github.context.repo.owner
 const repo: string = github.context.repo.repo
 
-function pullRequests(repoOwner:string, repo:string ):Promise<Octokit.Response<Octokit.PullsListResponse>> {
+function pullRequests(repoOwner:string, repo:string ) {
     let pr = new github.GitHub(token)
+    console.log(github)
     let resp = pr.pulls.list({
         owner: repoOwner,
         repo: repo,
     }).catch(
-        e => {
+        (e: any) => {
             console.log(e.message)
         }
-    ) as Promise<Octokit.Response<Octokit.PullsListResponse>>
-        console.log(pr)
+    )
+    console.log(resp)
     return resp
-}
-
-function filterTime(pull: Octokit.PullsListResponseItem,target: number):boolean{
-    const createdAt = Date.parse(pull.created_at)
-    const gapSec = Math.round((target - createdAt) / 1000)
-    if ( gapSec > skipSec ) {
-        return true
-    }
-    return false
 }
 
 pullRequests(repoOwner, repo);
